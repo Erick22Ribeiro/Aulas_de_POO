3921e9a10c27fffbd7fcb17c7c1423fd0041d872
Erick <erick2ribeirogg@gmail.com>
2026-03-10 14:02:22 -0300

    Add files via upload

--- POO - Aula 5/Petshop/controle/Programa.java
+++ POO - Aula 5/Petshop/controle/Programa.java
@@ -2,16 +2,14 @@ package controle;
 
 import dao.DaoCriacaoTabela;
 import view.CadastroAnimal;
-
-//Adicionar o driver ao projeto --> botão direito no projeto -> properties -> Java build path -> libraries -> ClassPath -> Add Externals Jars
-
+import view.ConsultaAnimal;
 
 public class Programa {
 	public static void main(String[] args) {
-		
-		DaoCriacaoTabela daoCriacaoTabela = new DaoCriacaoTabela();
-		CadastroAnimal cadastroAnimal = new CadastroAnimal();
-		
-		cadastroAnimal.setVisible(true);
+		//DaoCriacaoTabela daoCriacaoTabela = new DaoCriacaoTabela();
+		//CadastroAnimal cadastroAnimal = new CadastroAnimal();
+		//cadastroAnimal.setVisible(true);
+		ConsultaAnimal consultaAnimal = new ConsultaAnimal();
+		consultaAnimal.setVisible(true);
 	}
 }
